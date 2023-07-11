90828cb9371319934328a6d9689c3340e7144c37
Gabriel Lopes <123586763+GabrielLDN@users.noreply.github.com>
2023-07-11 12:56:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "banco_node"
+    database: "bd_node"
 });
 
 connection.connect((err) => {
